1b6a33ec490a5a1c1b867a50c27e59a71bbfa5b9
Alexey <tihonenko999@gmail.com>
2020-06-18 13:24:44 +0500

    contacts

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,30 +10,20 @@ export default {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: 'Ветеринар по вызову на дом. Помощь бездомным животным.'},
-      {hid: 'og-description', name: 'og:description', content: 'Ветеринар по вызову на дом. Помощь бездомным животным.'},
+      {
+        hid: 'og-description', name: 'og:description', content: 'Ветеринар по вызову на дом. Помощь бездомным животным.',
+      },
       // {hid: 'og-title', name: 'og:title', content: 'Ветеринар Ансар Шарипов'},
       {hid: 'og-image', name: 'og:image', content: 'https://veterinar-ufa.ru/img/ansar-sharipov.jpg'},
-    ],
-    link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
     ],
   },
   loading: {color: '#004679'},
   css: ['normalize.css', '~/assets/app.sass'],
-  /*
-  ** Plugins to load before mounting the App
-  */
   plugins: [],
-  /*
-  ** Nuxt.js dev-modules
-  */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
   ],
-  /*
-  ** Nuxt.js modules
-  */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
@@ -43,22 +33,8 @@ export default {
   styleResources: {
     sass: '~assets/_globals.sass',
   },
-  /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
   axios: {
     baseURL: process.env.NODE_ENV === 'production' ? 'https://veterinar-ufa.ru' : 'http://localhost:3000',
-  },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    },
   },
   generate: {
     routes: news.map(el => `/news/${el.id}`),
--- pages/contacts.vue
+++ pages/contacts.vue
@@ -1,12 +1,15 @@
 <template lang="pug">
   section.main
     .h1 Контакты
-    p Свяжитесь со мной через социальные сети или по номеру телефона. <br> На данный момент возможен только вызов на дом.
+    p: b Шарипов Ансар Радикович
+    p Ветеринарный врач, кандидат ветеринарных наук
     .socials
       a.item.__vk(href="https://vk.com/ansar72")
       a.item.__wa(href="https://api.whatsapp.com/send?phone=79174142891")
     a(href="tel:79174142891")
       img.phone(src="~/assets/img/phone.svg")
+    p г.Уфа, бульвар Хадии Давлетшиной 9, под №3, офис 6
+    iframe(src="https://yandex.ru/map-widget/v1/?um=constructor%3Af4ecef90d78629f5d3ae669f04d5857dfb9ff6db0d5db7c53b1160eab8900676&amp;source=constructor" width="100%" height="400" frameborder="0")
 </template>
 
 <script>
@@ -23,12 +26,15 @@ export default {
 .socials
   display: flex
   margin: 30px 0
+
   .item
     margin-right: 10px
     width: 51px
     height: 51px
+
     &.__vk
       background: url(~assets/img/vk-logo.svg)
+
     &.__wa
       background: url(~assets/img/wa-logo.svg)
 </style>
